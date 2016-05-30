cf0c8141f23c99b892f51fcefdb5448e0c8d4776
Willie Streeter <willie.l.streeter@kp.org>
2016-05-29 21:07:48 -0700

    Update with fixes to state

--- src/app/selectors/playlist.selector.ts
+++ src/app/selectors/playlist.selector.ts
@@ -14,38 +14,41 @@ const getAudioItem = (store: any) => store.select(STORE_SLICE_NAME)
 export const playlistArraySelector = (store: any) => store.select(STORE_SLICE_NAME)
                        .map(res => res.audioList);
 
-export const playlistArrayItemSelector = (store: any) => store.select(STORE_SLICE_NAME)
-                       .map(state => state.audioList).map(audioItem => audioItem.isPlaying).distinctUntilChanged();
+export const playlistSelector = (store: any) => store.select(STORE_SLICE_NAME);
 
 export const constructedPlaylistItem = (store: any) => Observable
-    .combineLatest(store.let(playlistArraySelector),
+    .combineLatest(store.let(playlistSelector),
                    store.let(artistSelector),
                    store.let(audioSelector),
-                   store.let(getAudioItem))
+                   store.let(playlistArraySelector))
     .map((res: any) => {
         console.log("[playlist.selector] --constructedPlaylistItem- playlistArraySelector --res[0] ", res[0]);
          // console.log("[playlist.selector] --constructedPlaylistItem- artistSelector --res[1] ", res[1]);
          // console.log("[playlist.selector] --constructedPlaylistItem- audioSelector --res[2] ", res[2]);
 
-        var playList = res[0];
-        if(playList.length>0 ){
+        var playlist =  Object.assign({}, res[0]);
+        console.log("[playlist.selector] --constructedPlaylistItem- playlistArraySelector --res[0].length "+ res[0].length);
+        console.log("[playlist.selector] --constructedPlaylistItem- playlistArraySelector --playlist.audioList.length = "+ playlist.audioList.length);
+
+        if(playlist.audioList.length>0 ){
             var artistAudioItem =  Object.keys(res[1]) .filter( x =>  {
                                       return res[1][x].id  ===  res[2].artistId})
                                      .map(x => {return res[1][x]})[0];
 
-            var newResult = Object.keys(playList).map(key => ({key:key, playlistItem:playList[key]})).map( value =>{
-               var obj;
-           //    console.log("playlist.selector] --constructedPlaylistItem --value.playlistItem.artistId", value.playlistItem.artistId);
-           //    console.log("playlist.selector] --constructedPlaylistItem --value.key", value.key);
-           //    console.log("playlist.selector] --constructedPlaylistItem --artistAudioItem.id", artistAudioItem.id);
+            var newResult = Object.keys(playlist.audioList).map(key => ({key:key, playlistItem:playlist.audioList[key]})).map( value =>{
+              // obj = res[0][value.key];
+              console.log("playlist.selector] --constructedPlaylistItem --value ",value);
+               console.log("playlist.selector] --constructedPlaylistItem --value.playlistItem.artistId", value.playlistItem.artistId);
+             // console.log("playlist.selector] --constructedPlaylistItem --artistAudioItem.id", artistAudioItem.id);
                if(value.playlistItem.artistId === artistAudioItem.id){
-                  playList[value.key]= Object.assign({}, value.playlistItem, {artist:artistAudioItem} );
+                  playlist.audioList[value.key] =  Object.assign({}, value.playlistItem, {artist:artistAudioItem} );
+                  //console.log("playlist.selector] --constructedPlaylistItem --   playList[value.key] =",  playList[value.key]);
                }
             });
-            // console.log("playlist.selector] --constructedPlaylistItem --valueer --playList ", playList);
-            return playList;
+            console.log("playlist.selector] --constructedPlaylistItem --valueer --playlist.audioList ", playlist.audioList);
+            return playlist.audioList;
         }else{
         console.log("[playlist.selector] -- constructedPlaylistItem -- DEFAULT");
-           return playList;
+           return res[3];
         }
     });
